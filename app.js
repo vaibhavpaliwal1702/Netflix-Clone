99d7852881c661cf857cdb9cb365f49224a50944
Vaibhav Paliwal <vaibahvpaliwal1702@gmail.com>
2023-06-22 15:18:25 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+$(function(){
+    var tabs = $('#features > nav a');
+    var tabsContent = $('#features > nav > article > section');
+
+    tabs.click(function(e){
+        e.preventDefault();
+
+        var that = $(this);
+
+        tabs.removeClass('is-selected');
+        that.addClass('is-selected');
+        tabsContent.removeClass('is-selected');
+        tabsContent.filter((i, tab) => $(tab).data('id') === that.data('id'))
+        .addClass('is-selected');
+    });
+});
